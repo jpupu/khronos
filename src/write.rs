9923ffeaf06bbf42b396f83b5a50ed2f770c35b8
Arto Hamara <arto@qem.fi>
2022-04-22 21:47:59 +0300

    feat: Add units for output formats

--- src/write.rs
+++ src/write.rs
@@ -1,16 +1,37 @@
 use chrono::NaiveDateTime;
 
+pub enum Unit {
+    Seconds,
+    Milliseconds,
+    Microseconds,
+    Nanoseconds,
+}
+
 pub enum OutputFormat {
     Iso8601,
-    Unix,
-    Delta,
+    Unix(Unit),
+    Delta(Unit),
 }
 
 pub fn write(format: OutputFormat, t: NaiveDateTime, prev_t: Option<NaiveDateTime>) -> String {
     match format {
         OutputFormat::Iso8601 => t.format("%Y-%m-%dT%H:%M:%S").to_string(),
-        OutputFormat::Unix => t.timestamp().to_string(),
-        OutputFormat::Delta => (t - prev_t.unwrap_or(t)).num_seconds().to_string(),
+        OutputFormat::Unix(Unit::Seconds) => t.timestamp().to_string(),
+        OutputFormat::Unix(Unit::Milliseconds) => t.timestamp_millis().to_string(),
+        OutputFormat::Unix(Unit::Microseconds) => (t.timestamp_nanos() / 1000).to_string(),
+        OutputFormat::Unix(Unit::Nanoseconds) => t.timestamp_nanos().to_string(),
+        OutputFormat::Delta(Unit::Seconds) => (t - prev_t.unwrap_or(t)).num_seconds().to_string(),
+        OutputFormat::Delta(Unit::Milliseconds) => {
+            (t - prev_t.unwrap_or(t)).num_milliseconds().to_string()
+        }
+        OutputFormat::Delta(Unit::Microseconds) => (t - prev_t.unwrap_or(t))
+            .num_microseconds()
+            .unwrap_or(0)
+            .to_string(),
+        OutputFormat::Delta(Unit::Nanoseconds) => (t - prev_t.unwrap_or(t))
+            .num_nanoseconds()
+            .unwrap_or(0)
+            .to_string(),
     }
 }
 
@@ -36,17 +57,59 @@ mod tests {
 
     #[test]
     fn output_unix() {
-        assert_eq!(write(OutputFormat::Unix, some_date(), None), "982240496");
+        assert_eq!(
+            write(OutputFormat::Unix(Unit::Seconds), some_date(), None),
+            "982240496"
+        );
+        assert_eq!(
+            write(OutputFormat::Unix(Unit::Milliseconds), some_date(), None),
+            "982240496123"
+        );
+        assert_eq!(
+            write(OutputFormat::Unix(Unit::Microseconds), some_date(), None),
+            "982240496123456"
+        );
+        assert_eq!(
+            write(OutputFormat::Unix(Unit::Nanoseconds), some_date(), None),
+            "982240496123456789"
+        );
     }
 
     #[test]
     fn output_delta() {
-        assert_eq!(write(OutputFormat::Delta, some_date(), None), "0");
+        assert_eq!(
+            write(OutputFormat::Delta(Unit::Seconds), some_date(), None),
+            "0"
+        );
         assert_eq!(
             write(
-                OutputFormat::Delta,
+                OutputFormat::Delta(Unit::Seconds),
                 some_date(),
                 Some(some_date() - Duration::seconds(130)),
+            ),
+            "130"
+        );
+        assert_eq!(
+            write(
+                OutputFormat::Delta(Unit::Milliseconds),
+                some_date(),
+                Some(some_date() - Duration::milliseconds(130)),
+            ),
+            "130"
+        );
+        assert_eq!(
+            write(
+                OutputFormat::Delta(Unit::Microseconds),
+                some_date(),
+                Some(some_date() - Duration::microseconds(130)),
+            ),
+            "130"
+        );
+        assert_eq!(
+            write(
+                OutputFormat::Delta(Unit::Nanoseconds),
+                some_date(),
+                Some(some_date() - Duration::nanoseconds(130)),
             ),
             "130"
         );
